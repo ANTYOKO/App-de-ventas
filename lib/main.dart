089f97969b0d50b174897fc45675b3a5581db95e
ANTYOKO <163502575+ANTYOKO@users.noreply.github.com>
2025-04-06 01:25:24 -0600

    Create main.dart

---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+name: app_ventas
+description: Aplicación de ventas con Flutter y Firebase
+publish_to: 'none'
+version: 1.0.0+1
+
+environment:
+  sdk: ">=2.17.0 <3.0.0"
+
+dependencies:
+  flutter:
+    sdk: flutter
+  cupertino_icons: ^1.0.2
+  firebase_core: ^2.15.0
+  firebase_auth: ^4.7.2
+  cloud_firestore: ^4.8.4
+  provider: ^6.0.5
+
+dev_dependencies:
+  flutter_test:
+    sdk: flutter
+  flutter_lints: ^2.0.0
+
+flutter:
+  uses-material-design: true
